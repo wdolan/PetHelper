1411fe94135a4626a8687bb58581f5ec41e98100
wdolan <thebilldolan@gmail.com>
2014-02-17 22:19:07 -0500

    comment testing fun

--- demo.php
+++ demo.php
@@ -5,6 +5,7 @@
 <!-- This will contain the hello and login and create that will be first used-->
 <!-- First page to be loaded and where it will start from.-->
 <!-- Better comment here.-->
+<!-- Second test comment-->
 
 <?php
 
